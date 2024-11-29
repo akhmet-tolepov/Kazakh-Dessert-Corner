a321c990f0227491179495ec66fb4bc50cd3129c
akhmet-tolepov <zhakiya190167@gmail.com>
2024-11-29 22:10:57 +0500

    meeting

---
+++ Kazakh Dessert Corner/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  Kazakh Dessert Corner
+//
+//  Created by Akhmet Tolepov on 29.11.2024.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
